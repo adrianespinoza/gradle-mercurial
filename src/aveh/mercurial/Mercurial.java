cc89ac1c06159c0f6567398392bee5bbb0ac03b2
Adrian Espinoza <ahuanca@pros.com>
2018-01-17 09:04:24 -0400

    POC of mercurial with gradle

---
+++ src/aveh/mercurial/Mercurial.java
@@ -0,0 +1,14 @@
+package aveh.mercurial;
+
+import java.util.List;
+
+public class Mercurial {
+    public static void execHG() {
+        List<String> cmdLine;
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String[] d = {"d"};
+        int l = d.length;
+        String r = "";
+        r.isEmpty();
+    }
+}
